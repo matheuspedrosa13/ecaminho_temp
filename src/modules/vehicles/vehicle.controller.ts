c4325900670c4084af4f13922ef689e338ab5357
Henry Gois Ribeiro <henry.ribeiro@germinare.org.br>
2025-01-30 00:26:01 -0300

    finalizando a rota completa de veículo

--- src/modules/vehicles/vehicle.controller.ts
+++ src/modules/vehicles/vehicle.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, HttpException, HttpStatus } from '@nestjs/common';
 import { VehiclesService } from './vehicle.service';
 import { CreateVehicleDto } from './dto/createvehicle.dto';
 import { UpdateVehicleDto } from './dto/updatevehicle.dto';
-import { SkipAuth } from 'src/constants';
 import { HttpResponse } from 'src/shared/interfaces/http-response.interface';
+import { eca_vehicles as Vehicle } from '@prisma/client'; 
+
 
 
 @Controller('vehicles')
@@ -11,37 +12,63 @@ export class VehiclesController {
   constructor(private readonly vehiclesService: VehiclesService) {}
 
   @Post()
-  async create(@Body() createVehicleDto: CreateVehicleDto, @Request() req) : Promise<HttpResponse<boolean>>{
-    const userReq = req.user;
+async create(@Body() createVehicleDto: CreateVehicleDto, @Request() req): Promise<HttpResponse<boolean>> {
+  const userReq = req.user;
+  try {
     await this.vehiclesService.create(createVehicleDto, userReq.id);
-    const response: HttpResponse<boolean> = {
+    
+    return {
       data: true,
-      message: "Vehicle created successfully"
+      message: "vehicle created successfully",
+    };
+  } catch (error) {
+    if (error.response) {
+      throw new HttpException(error.response, HttpStatus.CONFLICT);
+    }
+    throw new HttpException({
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+      error: 'Error creating vehicle',
+    }, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+ @Get()
+async findAll(): Promise<HttpResponse<Vehicle[]>> {
+  const result = await this.vehiclesService.findAll();
+  const response: HttpResponse<Vehicle[]> = { 
+    data: result,
+    message: "success"
+  };
+  return response;
+}
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) : Promise<HttpResponse<Vehicle>>{
+    const result = await this.vehiclesService.findOne(id);
+    const response: HttpResponse<Vehicle> = {
+      data: result,
+      message: "success"
     }
     return response
   }
 
-  @Get()
-  @SkipAuth()
-  async findAll() {
-    return await this.vehiclesService.findAll();
-  }
-
-  @Get(':id')
-  @SkipAuth()
-  async findOne(@Param('id') id: number) {
-    return await this.vehiclesService.findOne(id);
-  }
-
   @Patch(':id')
-  @SkipAuth()
-  async update(@Param('id') id: number, @Body() updateVehicleDto: UpdateVehicleDto) {
-    return await this.vehiclesService.update(id, updateVehicleDto);
+  async update(@Param('id') id: number, @Body() updateVehicleDto: UpdateVehicleDto) : Promise<HttpResponse<Vehicle>>{
+    const result = await this.vehiclesService.update(id, updateVehicleDto);
+    const response: HttpResponse<Vehicle> = {
+      data: result,
+      message: "success"
+    }
+    return response
   }
 
   @Delete(':id')
-  @SkipAuth()
-  async remove(@Param('id') id: number) {
-    return await this.vehiclesService.remove(id);
+  async remove(@Param('id') id: number) : Promise<HttpResponse<boolean>>{
+    await this.vehiclesService.remove(id);
+    const response: HttpResponse<boolean> = {
+      data: true,
+      message: "success"
+    }
+    return response
   }
 }
--- src/modules/vehicles/vehicle.service.ts
+++ src/modules/vehicles/vehicle.service.ts
@@ -1,37 +1,52 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { CreateVehicleDto } from './dto/createvehicle.dto';
 import { UpdateVehicleDto } from './dto/updatevehicle.dto';
+import { eca_vehicles as Vehicle } from '@prisma/client'; 
 
 @Injectable()
 export class VehiclesService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createVehicleDto: CreateVehicleDto, userId: number) {
+  async create(createVehicleDto: CreateVehicleDto, userId: number): Promise<Vehicle> {
+    const plate = await this.prisma.eca_vehicles.findUnique({
+      where: { plate: createVehicleDto.plate },
+    });
+    
+    if (plate) 
+      throw new HttpException("plate already exists", HttpStatus.CONFLICT);
+    
     const result = await this.prisma.eca_vehicles.create({
       data: createVehicleDto,
     });
-    await this.prisma.eca_users.update({where: {pk_id : userId}, data: {pfk_eca_vehicles_id : result.pk_id}} )
+
+    await this.prisma.eca_users.update({
+      where: { pk_id: userId },
+      data: { pfk_eca_vehicles_id: result.pk_id }
+    });
+
+    return result;
   }
 
-  async findAll() {
+  async findAll(): Promise<Vehicle[]> {
     return await this.prisma.eca_vehicles.findMany({
       include: { eca_users: true }, 
     });
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<Vehicle> {
     const vehicle = await this.prisma.eca_vehicles.findUnique({
       where: { pk_id: id },
       include: { eca_users: true }, 
     });
-    if (!vehicle) {
+
+    if (!vehicle) 
       throw new NotFoundException(`${id} not found`);
-    }
+
     return vehicle;
   }
 
-  async update(id: number, updateVehicleDto: UpdateVehicleDto) {
+  async update(id: number, updateVehicleDto: UpdateVehicleDto): Promise<Vehicle> {
     await this.findOne(id); 
     return await this.prisma.eca_vehicles.update({
       where: { pk_id: id },
@@ -39,7 +54,7 @@ export class VehiclesService {
     });
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<Vehicle> {
     await this.findOne(id); 
     return await this.prisma.eca_vehicles.delete({
       where: { pk_id: id },
