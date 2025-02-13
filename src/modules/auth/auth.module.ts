9bcbf61971957458aa13ad70849dccb92392b560
Lucas Aquino <99625411+LucassAquino@users.noreply.github.com>
2025-02-13 09:53:16 -0300

    Update auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/modules/user/user.module';
-import { AuthGuard } from '../../shared/guards/auth.guard';
 
 @Module({
   imports: [UserModule],
   controllers: [AuthController],
-  providers: [AuthGuard, AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
