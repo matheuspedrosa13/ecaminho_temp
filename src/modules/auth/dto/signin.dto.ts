768001bf5699eae203545358d5b9ff38fba64685
Lucas Aquino <99625411+LucassAquino@users.noreply.github.com>
2025-02-13 09:54:07 -0300

    Update signin.dto.ts
    adding swagger properties

--- src/modules/auth/dto/signin.dto.ts
+++ src/modules/auth/dto/signin.dto.ts
@@ -1,4 +1,19 @@
-export interface SigninDto {
-    email: string,
-    password: string
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MaxLength } from 'class-validator';
+
+export class SigninDto {
+  @ApiProperty({
+    description: "User's e-mail to generate a new jwt token",
+    example: 'lucas.aquino@germinare.org.br',
+  })
+  @IsString()
+  @MaxLength(255)
+  email: string;
+
+  @ApiProperty({
+    description: "User's password to generate a new jwt token",
+    example: 'Senha123*',
+  })
+  @IsString()
+  password: string;
 }
